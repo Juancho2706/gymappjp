ea51962e467269d8a89e4ae79da7c56166c0a189
Juancho2706 <jvillegas.dev@gmail.com>
2026-04-15 21:25:10 -0400

    fix(ClientExerciseCatalog): use local variable for selectedExercise in YouTube embed URL

--- src/app/c/[coach_slug]/exercises/ClientExerciseCatalog.tsx
+++ src/app/c/[coach_slug]/exercises/ClientExerciseCatalog.tsx
@@ -194,10 +194,11 @@ export function ClientExerciseCatalog({ byMuscle, primaryColor }: Props) {
 
                 if (isYouTube) {
                   const ytId = getYouTubeId(url!);
+                  const ex = selectedExercise as any
                   const embedUrl = ytId ? `https://www.youtube-nocookie.com/embed/${ytId}?autoplay=1&mute=1&loop=1&playlist=${ytId}&modestbranding=1&rel=0&showinfo=0&controls=1${
-                    selectedExercise.video_start_time ? `&start=${selectedExercise.video_start_time}` : ''
+                    ex.video_start_time ? `&start=${ex.video_start_time}` : ''
                   }${
-                    selectedExercise.video_end_time ? `&end=${selectedExercise.video_end_time}` : ''
+                    ex.video_end_time ? `&end=${ex.video_end_time}` : ''
                   }` : '';
 
                   return ytId ? (
