d674ff0fd16649b103fcc7fcd9b4b37dacebf9ab
Juan Villegas <jvillegas.dev@gmail.com>
2026-07-14 14:47:50 -0400

    fix(nutrition-v2): export canonical web overrides

--- apps/web/src/components/nutrition-v2/index.ts
+++ apps/web/src/components/nutrition-v2/index.ts
@@ -1,2 +1,23 @@
-export * from './NutritionV2Kit'
+export {
+  BuilderInspector,
+  BuilderStepList,
+  CoachAttentionCard,
+  FoodRow,
+  FoodThumbnail,
+  MealSlotCard,
+  MealTimeline,
+  NutritionCard,
+  NutritionHeader,
+  NutritionPageShell,
+  NutritionRefreshButton,
+  NutritionSkeleton,
+  NutritionStatePanel,
+  NutritionToolbar,
+  PlanVersionBadge,
+  ResponsiveDataAdapter,
+  StrategyBadge,
+  SyncOfflineState,
+  type NutritionCardProps,
+} from './NutritionV2Kit'
+export { MacroBudget, MacroProgress, StudentPreview } from './NutritionV2Overrides'
 export * from './NutritionV2Motion'
